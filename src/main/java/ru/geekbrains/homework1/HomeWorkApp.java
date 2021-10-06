d5b2bde6a3f54ef7f807c977d42493c17fc4f65c
AlinaArakelova <danikalina@gmail.com>
2021-10-06 18:41:37 +0300

    Задание 3
    Задание 3

--- src/main/java/ru/geekbrains/homework1/HomeWorkApp.java
+++ src/main/java/ru/geekbrains/homework1/HomeWorkApp.java
@@ -1,8 +1,56 @@
 package ru.geekbrains.homework1;
 
+import java.util.Arrays;
+
 public class HomeWorkApp {
     public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 0, 1, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+
+
+        int[] arr1 = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr1[i] = i + 1;
+        }
+        System.out.println(Arrays.toString(arr1));
+
+
+        int[] arr2 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr2.length; i++) {
+            if (arr2[i] < 6) {
+                arr2[i] = arr2[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr2));
+
+        int rows = 5;
+        int cols = 5;
+        int[][] arr3 = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            arr3[i][i] = 1;
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(arr3[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 
-
+    public static int[] myMethod(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = initialValue;
+        }
+        return arr;
+    }
 }
